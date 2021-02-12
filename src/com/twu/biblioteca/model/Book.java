f7245f27306bbb5dfdfafb93e26add97f935654a
NatashaZettler-TW <natasha.zettler@thoughtworks.com>
2021-02-12 00:12:41 -0300

    feat(Book): override equals method

--- src/com/twu/biblioteca/model/Book.java
+++ src/com/twu/biblioteca/model/Book.java
@@ -1,16 +1,26 @@
 package com.twu.biblioteca.model;
 
+import java.util.Objects;
+
 public class Book {
-    private String name;
+    private String title;
     private String author;
 
-    public Book(String name, String author) {
-        this.name = name;
+    public Book(String title, String author) {
+        this.title = title;
         this.author = author;
     }
 
     @Override
     public String toString(){
-        return "name: "+ this.name + "\nauthor: "+this.author;
+        return "title: "+ this.title + "\nauthor: "+this.author;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Book)) return false;
+        Book book = (Book) o;
+        return Objects.equals(title, book.title) && Objects.equals(author, book.author);
     }
 }

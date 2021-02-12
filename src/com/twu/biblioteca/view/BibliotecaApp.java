6665f500530b650de1995922532c63fdb9d3b291
NatashaZettler-TW <natasha.zettler@thoughtworks.com>
2021-02-12 02:59:19 -0300

    refactor(Interface): change the interface to include injetion dependence

--- src/com/twu/biblioteca/view/BibliotecaApp.java
+++ src/com/twu/biblioteca/view/BibliotecaApp.java
@@ -1,18 +1,42 @@
 package com.twu.biblioteca.view;
 
+import com.twu.biblioteca.controller.ILibrary;
 import com.twu.biblioteca.controller.LibraryController;
 import com.twu.biblioteca.model.Book;
 
 public class BibliotecaApp {
 
-    public static void main(String[] args) {
-
-        System.out.println("Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!");
-        LibraryController libraryController = new LibraryController();
-        System.out.println("List of books:");
-        for(Book book : libraryController.bookList()){
-            System.out.println("- "+book.toString());
+    public static int menu(ICommandLine system){
+        while(true) {
+            try {
+                system.println("\nChoose one option");
+                system.println("\t1 - List of books");
+                return system.scanner();
+            } catch (Exception exception) {
+                system.println("Ops. Something went wrong. Try again.");
+            }
         }
+    }
 
+    public static void option(int menu, ILibrary libraryController, ICommandLine system){
+        switch(menu){
+            case 1:
+                for(Book book : libraryController.bookList()){
+                    system.println("- "+book.toString());
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+    public static void main(String[] args) {
+        SystemCommandLine system = new SystemCommandLine();
+        welcomeMessage(system);
+        option(menu(system), new LibraryController(), system);
+    }
+
+    public static void welcomeMessage(ICommandLine system){
+        system.println("Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!");
     }
 }

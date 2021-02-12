6da2a1b141c05e459bc15fcea4d3e71257be7102
NatashaZettler-TW <natasha.zettler@thoughtworks.com>
2021-02-12 13:12:53 -0300

    test(Interface): include tests to functions borrow and return book

--- test/com/twu/biblioteca/view/BibliotecaAppTest.java
+++ test/com/twu/biblioteca/view/BibliotecaAppTest.java
@@ -13,15 +13,17 @@ import static org.junit.Assert.assertEquals;
 
 public class BibliotecaAppTest {
 
-    MockCommandLine mock;
+    MockCommandLine mockCommandLine;
+    MockLibrary mockLibrary;
 
     @Before
     public void setUp(){
-        mock = new MockCommandLine();
+        mockCommandLine = new MockCommandLine();
+        mockLibrary = new MockLibrary();
     }
     @Test
     public void shouldReturnOptionListBook(){
-        assertEquals(12, BibliotecaApp.menu(new MockCommandLine()));
+        assertEquals(12, BibliotecaApp.menu(mockCommandLine));
     }
 
     @Test
@@ -30,33 +32,37 @@ public class BibliotecaAppTest {
                 "- Title: Livro1\tAuthor: Autor1\tPublication Year: 2021",
                 "- Title: Livro2\tAuthor: Autor2\tPublication Year: 2019");
 
-        mock.scanValue = 0;
-        BibliotecaApp.option(1, new MockLibrary(), mock);
-        assertEquals(expectedMessages.get(0), mock.messageList.get(0));
-        assertEquals(expectedMessages.get(1), mock.messageList.get(1));
+        mockCommandLine.scanValue = 0;
+        BibliotecaApp.option(1, mockLibrary, mockCommandLine);
+        assertEquals(expectedMessages.get(0), mockCommandLine.messageList.get(0));
+        assertEquals(expectedMessages.get(1), mockCommandLine.messageList.get(1));
     }
 
     static class MockLibrary implements ILibrary {
+        public boolean isBorrow = false;
+        public boolean returnBook = false;
+
         @Override
         public List<Book> bookList() {
-            return Arrays.asList(new Book("Livro1","Autor1", 2021),
-                    new Book("Livro2","Autor2", 2019));
+           return Arrays.asList(new Book("Livro1","Autor1", 2021),
+                   new Book("Livro2","Autor2", 2019));
         }
 
         @Override
         public boolean borrow(String title) {
-            return false;
+            return isBorrow;
         }
 
         @Override
         public boolean returnBook(String title) {
-            return false;
+            return returnBook;
         }
     }
 
     static class MockCommandLine implements ICommandLine {
         public List<String> messageList = new ArrayList<>();
         public int scanValue = 12;
+        public String scanValueText = "livro1";
         @Override
         public void println(String message) {
             messageList.add(message);
@@ -69,21 +75,55 @@ public class BibliotecaAppTest {
 
         @Override
         public String scannerText() {
-            return "Text";
+            return scanValueText;
         }
     }
 
     @Test
     public void shouldShowMessageWhenUserChooseInvalidOption(){
-        mock.scanValue = 0;
-        BibliotecaApp.option(10, new MockLibrary(), mock);
-        assertEquals("Please select a valid option!",mock.messageList.get(0));
+        mockCommandLine.scanValue = 0;
+        BibliotecaApp.option(10, mockLibrary, mockCommandLine);
+        assertEquals("Please select a valid option!",mockCommandLine.messageList.get(0));
     }
 
     @Test
     public void shouldShowMessageWhenUserChooseQuitApplication(){
-        BibliotecaApp.option(0, new MockLibrary(), mock);
-        assertEquals("Thank you for use the system. Come back soon.",mock.messageList.get(0));
+        BibliotecaApp.option(0, mockLibrary, mockCommandLine);
+        assertEquals("Thank you for use the system. Come back soon.",mockCommandLine.messageList.get(0));
+    }
+
+    @Test
+    public void shouldShowSuccessMessageWhenUserChooseReturnBook(){
+        mockCommandLine.scanValue = 0;
+        mockCommandLine.scanValueText = "livro1";
+        mockLibrary.returnBook = true;
+        BibliotecaApp.option(3, mockLibrary, mockCommandLine);
+        assertEquals("Thank you for returning the book.",mockCommandLine.messageList.get(1));
+    }
+
+    @Test
+    public void shouldShowMessageBookIsNotValidWhenUserChooseReturnBookThatAlreadyIsInTheLibrary(){
+        mockCommandLine.scanValue = 0;
+        mockCommandLine.scanValueText = "livro2";
+        BibliotecaApp.option(3, mockLibrary, mockCommandLine);
+        assertEquals("That is not a valid book to return.",mockCommandLine.messageList.get(1));
+    }
+
+    @Test
+    public void shouldShowSuccessMessageWhenUserChooseBorrowBook(){
+        mockCommandLine.scanValue = 0;
+        mockCommandLine.scanValueText = "livro1";
+        mockLibrary.isBorrow = true;
+        BibliotecaApp.option(2, mockLibrary, mockCommandLine);
+        assertEquals("Thank you! Enjoy the book.", mockCommandLine.messageList.get(1));
+    }
+
+    @Test
+    public void shouldShowMessageBookIsNotValidWhenUserChooseReturnBookThatAlreadyIsTheLibrary(){
+        mockCommandLine.scanValue = 0;
+        mockCommandLine.scanValueText = "livro2";
+        BibliotecaApp.option(2, mockLibrary, mockCommandLine);
+        assertEquals("Sorry, that book is not available.",mockCommandLine.messageList.get(1));
     }
 }
 

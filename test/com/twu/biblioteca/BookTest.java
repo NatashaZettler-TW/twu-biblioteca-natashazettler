17ac786434c3eefb6010ca796054abd2e9e8143d
NatashaZettler-TW <natasha.zettler@thoughtworks.com>
2021-02-12 00:14:22 -0300

    feat(BookTest): include test for equals method

--- test/com/twu/biblioteca/BookTest.java
+++ test/com/twu/biblioteca/BookTest.java
@@ -2,12 +2,34 @@ package com.twu.biblioteca;
 
 import com.twu.biblioteca.model.Book;
 import org.junit.Test;
-import static org.junit.Assert.assertEquals;
+
+import static org.junit.Assert.*;
 
 public class BookTest {
     @Test
-    public void shouldPrintWhenCallToStringMethod() {
+    public void shouldReturnBookInformationWhenCallToStringMethod() {
         Book book = new Book("Livro1","Autor1");
         assertEquals("name: Livro1\nauthor: Autor1", book.toString());
+    }
+
+    @Test
+    public void shouldCompareBooksWhenCallEqualsMethod(){
+        Book book = new Book("Livro1","Autor1");
+        Book otherBook = new Book("Livro1","Autor1");
+        assertTrue(book.equals(otherBook));
+    }
+
+    @Test
+    public void shouldReturnFalseWhenCompareTitleBooks(){
+        Book book = new Book("Livro2","Autor1");
+        Book otherBook = new Book("Livro1","Autor1");
+        assertFalse(book.equals(otherBook));
+    }
+
+    @Test
+    public void shouldReturnFalseWhenCompareAuthorBooks(){
+        Book book = new Book("Livro1","Autor2");
+        Book otherBook = new Book("Livro1","Autor1");
+        assertFalse(book.equals(otherBook));
     }
 }

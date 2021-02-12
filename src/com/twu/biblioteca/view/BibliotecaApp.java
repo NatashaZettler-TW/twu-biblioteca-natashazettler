485d02d10605ffbd05613f01bc11dda3566184bc
NatashaZettler-TW <natasha.zettler@thoughtworks.com>
2021-02-12 05:13:48 -0300

    feat(Option): include new option to return book

--- src/com/twu/biblioteca/view/BibliotecaApp.java
+++ src/com/twu/biblioteca/view/BibliotecaApp.java
@@ -12,6 +12,7 @@ public class BibliotecaApp {
                 system.println("\nChoose one option");
                 system.println("\t1 - List of books");
                 system.println("\t2 - Borrow book");
+                system.println("\t3 - Return book");
                 system.println("\t0 - Quit");
                 return system.scanner();
             } catch (Exception exception) {
@@ -33,6 +34,14 @@ public class BibliotecaApp {
                     system.println("Thank you! Enjoy the book");
                 } else {
                     system.println("Sorry, that book is not available");
+                }
+                break;
+            case 3:
+                system.println("Write the book's name and press enter");
+                if (libraryController.returnBook(system.scannerText())) {
+                    system.println("Thank you for returning the book");
+                } else {
+                    system.println("That is not a valid book to return.");
                 }
                 break;
             case 0:

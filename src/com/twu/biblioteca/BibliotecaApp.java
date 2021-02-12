ffa9c561ee43a4683eeae864fbe9e6ff49eb9f71
NatashaZettler-TW <natasha.zettler@thoughtworks.com>
2021-02-12 00:20:44 -0300

    feat(BookList): show book list

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -1,9 +1,13 @@
 package com.twu.biblioteca;
 
+import com.twu.biblioteca.controller.LibraryController;
+
 public class BibliotecaApp {
 
     public static void main(String[] args) {
 
         System.out.println("Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!");
+        LibraryController libraryController = new LibraryController();
+        System.out.println(libraryController.bookList().toString());
     }
 }

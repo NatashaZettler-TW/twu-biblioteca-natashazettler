80b3789a8fc0bb99f6419fe4ff92369a9c5bcfac
NatashaZettler-TW <natasha.zettler@thoughtworks.com>
2021-02-12 04:46:09 -0300

    feat(Book): include borrowed attribute

--- src/com/twu/biblioteca/model/Book.java
+++ src/com/twu/biblioteca/model/Book.java
@@ -6,11 +6,13 @@ public class Book {
     private String title;
     private String author;
     private int publicationYear;
+    private Boolean borrowed;
 
     public Book(String title, String author, int publicationYear) {
         this.title = title;
         this.author = author;
         this.publicationYear = publicationYear;
+        this.borrowed = false;
     }
 
     @Override
@@ -24,5 +26,17 @@ public class Book {
         if (!(o instanceof Book)) return false;
         Book book = (Book) o;
         return publicationYear == book.publicationYear && Objects.equals(title, book.title) && Objects.equals(author, book.author);
+    }
+
+    public Boolean getBorrowed() {
+        return borrowed;
+    }
+
+    public void setBorrowed(Boolean borrowed) {
+        this.borrowed = borrowed;
+    }
+
+    public String getTitle() {
+        return title;
     }
 }
